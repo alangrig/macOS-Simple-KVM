4caebdb5a6753d01ace446e1b9d1647b37283860
Matisse Babonneau <32793760+Matbabs@users.noreply.github.com>
2023-10-25 19:36:17 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,14 +3,12 @@
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VMDIR=$PWD
 OVMF=$VMDIR/firmware
-#export QEMU_AUDIO_DRV=pa
-#QEMU_AUDIO_DRV=pa
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 24G \
+    -m 16G \
     -machine q35,accel=kvm \
-    -smp $(nproc) \
+    -smp 8,cores=4 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
@@ -20,7 +18,7 @@ qemu-system-x86_64 \
     -device ich9-intel-hda -device hda-output \
     -usb -device usb-kbd -device usb-mouse \
     -netdev user,id=net0 \
-    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -device e1000-82545em,netdev=net0,id=net0,mac=e5:c5:ee:8f:db:d5 \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
