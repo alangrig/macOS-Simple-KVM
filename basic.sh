75e3719feaba06adf9e032afb9bd1c31af1446bb
Matisse Babonneau <32793760+Matbabs@users.noreply.github.com>
2023-10-26 18:22:01 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,12 +3,12 @@
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VMDIR=$PWD
 OVMF=$VMDIR/firmware
-QEMU_AUDIO_DRV=pa
+# QEMU_AUDIO_DRV=pa
 
 qemu-system-x86_64 \
     -nodefaults \
     -enable-kvm \
-    -m 16G \
+    -m 24G \
     -machine q35,accel=kvm \
     -smp 8,cores=4 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
@@ -16,7 +16,7 @@ qemu-system-x86_64 \
     -smbios type=2 \
     -drive if=pflash,format=raw,readonly,file="$OVMF/OVMF_CODE.fd" \
     -drive if=pflash,format=raw,file="$OVMF/OVMF_VARS-1024x768.fd" \
-    -vga std \
+    -vga qxl \
     -device ich9-intel-hda -device hda-output \
     -usb -device usb-kbd -device usb-mouse \
     -netdev user,id=net0 \
